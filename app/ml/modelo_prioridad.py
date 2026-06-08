766d531a3d4a2f86d8fdc43ce426db1d2e40fda2
ChristianAguayo97 <christianezequiel783@gmail.com>
2026-06-08 00:54:29 +0000

    error en modelo

--- app/ml/modelo_prioridad.py
+++ app/ml/modelo_prioridad.py
@@ -1,21 +1,33 @@
 import pandas as pd
 import joblib
 import os
+import sys
 from app.models.entidades import PrioridadEnvio
 
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__)))  # Esto es la carpeta 'app'
-ROOT_DIR = os.path.dirname(BASE_DIR)  # Esto es la raíz del proyecto 'CompuYa'
-MODEL_PATH = os.path.join(ROOT_DIR, 'modelo_prioridad.joblib')
-ENCODER_PATH = os.path.join(ROOT_DIR, 'label_encoder.joblib')
+def get_resource_path(relative_path):
+    if getattr(sys, 'frozen', False):
+        # Si es un binario de PyInstaller
+        base_path = sys._MEIPASS
+    else:
+        # Modo desarrollo
+        base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    
+    return os.path.join(base_path, relative_path)
+
+MODEL_PATH = get_resource_path('modelo_prioridad.joblib')
+ENCODER_PATH = get_resource_path('label_encoder.joblib')
 
 try:
-    model = joblib.load(MODEL_PATH)
-    label_encoder = joblib.load(ENCODER_PATH)
-    print("Modelo de prioridad y codificador cargados exitosamente.")
-except FileNotFoundError:
-    print(
-        f"Error: No se encontraron los archivos del modelo en {MODEL_PATH} o {ENCODER_PATH}")
+    if os.path.exists(MODEL_PATH) and os.path.exists(ENCODER_PATH):
+        model = joblib.load(MODEL_PATH)
+        label_encoder = joblib.load(ENCODER_PATH)
+        print("Modelo de prioridad y codificador cargados exitosamente.")
+    else:
+        print(f"Error: No se encontraron los archivos del modelo en {MODEL_PATH}")
+        model = None
+        label_encoder = None
+except Exception as e:
+    print(f"Error al cargar el modelo: {str(e)}")
     model = None
     label_encoder = None
 
