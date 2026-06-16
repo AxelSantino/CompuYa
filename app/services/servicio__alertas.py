637d2a52434befbe9f570efe0dd1d67b3be678df
natan365l <natanaelandresotranto@gmail.com>
2026-06-16 17:19:26 -0300

    feat: agregar websockets para alertas en tiempo real

--- app/services/servicio__alertas.py
+++ app/services/servicio__alertas.py
@@ -1,7 +1,10 @@
 import httpx
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.entidades import NotificacionApp
-
+import httpx
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.entidades import NotificacionApp
+from app.utils.ws_manager import manager  
 
 async def crear_alerta_y_enviar_push(db: AsyncSession, usuario_id: int, titulo: str, mensaje: str, envio_id: int = None):
     nueva_alerta = NotificacionApp(usuario_id=usuario_id, titulo=titulo, mensaje=mensaje, envio_id=envio_id)
@@ -18,5 +21,14 @@ async def crear_alerta_y_enviar_push(db: AsyncSession, usuario_id: int, titulo: 
             await client.post(url, data=mensaje.encode('utf-8'), headers=headers)
         except Exception as e:
             print(f"Error push: {e}")
+            
+    datos_ws = {
+        "tipo": "NUEVA_ALERTA",
+        "titulo": titulo,
+        "mensaje": mensaje,
+        "envio_id": envio_id
+    }
+    
+    await manager.enviar_mensaje_personal(datos_ws, str(usuario_id))
             
     return nueva_alerta
